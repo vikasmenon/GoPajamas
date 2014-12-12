183bca1955b7669448e7c0a5fd91f7b697260e36
Vikas Menon <menonvikas@gmail.com>
2014-12-11 21:29:38 -0500

    first commit

---
+++ test.hs
@@ -0,0 +1,8 @@
+foo x = x*x
+
+fooBar y = y + foo y
+
+
+main = do line <- fmap reverse getLine  
+          putStrLn $ "You said " ++ line ++ " backwards!"  
+          putStrLn $ "Yes, you really said" ++ line ++ " backwards!"  
