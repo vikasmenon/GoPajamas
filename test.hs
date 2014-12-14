5a5161023f6d09b40cfb5d16b4607489619023af
Anmol Tiwari <menonvikas@gmail.com>
2014-12-14 13:39:48 -0500

    adding solver for reverse words

--- test.hs
+++ test.hs
@@ -1,7 +1,23 @@
 import Data.List (intercalate)
 import Data.List.Split (splitOn)
+import System.Environment
 
+solve :: [Char] -> [Char]
 solve x = intercalate " " $ reverse $ splitOn " " x
 
+boilerPlate :: [[Char]]
+boilerPlate = ["Case #" ++ show n ++ ": " |n <- [1..]]
+  
+standardOutput :: [[Char]] -> [[Char]]
+standardOutput = zipWith (++) boilerPlate
 
+main :: IO ()
+main = do
+  (f:_) <- getArgs
+  file <- readFile f
+  let puzzles   = lines file
+      solutions = standardOutput $ map solve puzzles
+  putStrLn $ unlines $ solutions
+
+  undefined
 
