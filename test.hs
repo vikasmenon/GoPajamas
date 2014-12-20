19e0b913b68c8b27ad0f0a0507e57060a2c52b35
Anmol Tiwari <menonvikas@gmail.com>
2014-12-20 17:59:06 -0500

    solving the welcome problem

--- test.hs
+++ test.hs
@@ -12,12 +12,37 @@ boilerPlate = ["Case #" ++ show n ++ ": " |n <- [1..]]
 standardOutput :: [[Char]] -> [[Char]]
 standardOutput = zipWith (++) boilerPlate
 
-parallel_solve puzzles = parMap rpar solve puzzles
+parallel_solve func puzzles = parMap rpar func puzzles
+
+get_puzzles fileHandle = tail $ lines fileHandle
+
+
+fib 0 = 1
+fib 1 = 1
+fib n = fib (n-2) + fib (n-1)
+
+lst = map (+5) [30, 33, 32, 30, 3, 0, 1]
+
+serial_order = map fib lst 
+test_order = parMap rpar fib lst
+-- test should assert serial_order == test_order irrespective of number of cores
 
 main :: IO ()
 main = do
   (f:_) <- getArgs
-  file <- readFile f
-  let puzzles   = tail $ lines file
-      solutions = standardOutput $ parallel_solve puzzles 
-  putStrLn $ unlines $ solutions
+  file <- readFile f   
+  --putStrLn $ unlines $ standardOutput $ parallel_solve solve ( get_puzzles file)
+  putStrLn $ unlines $ standardOutput $ parallel_solve format_solve ( get_puzzles file)
+
+welcome_solve sub_x [] = 0
+welcome_solve (sub_x: []) str_xs = length ( filter (\x -> x==sub_x) str_xs ) 
+welcome_solve (sub_x:sub_xs) (str_x:str_xs) = if sub_x==str_x
+                                                 then (welcome_solve sub_xs str_xs)  + (welcome_solve (sub_x:sub_xs) str_xs) 
+                                                 else welcome_solve (sub_x:sub_xs) str_xs
+
+getLast4 (w:x:y:z: []) = [w,x,y,z]
+getLast4 (x:xs) = getLast4 xs
+format_solve problem 
+             | solution < 1000 = getLast4 $ show $ 10000 + solution
+             | otherwise  = getLast4 $ show solution
+             where solution = welcome_solve "welcome to code jam" problem
