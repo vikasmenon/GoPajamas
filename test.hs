82c94304499d86e5618720493311d1b2c6b0b644
Vikas Menon <menonvikas@gmail.com>
2015-01-18 13:58:41 -0500

    attempting alien solve

--- test.hs
+++ test.hs
@@ -31,8 +31,9 @@ main :: IO ()
 main = do
   (f:_) <- getArgs
   file <- readFile f   
-  --putStrLn $ unlines $ standardOutput $ parallel_solve solve ( get_puzzles file)
-  putStrLn $ unlines $ standardOutput $ parallel_solve format_solve ( get_puzzles file)
+  --putStrLn $ unlines $ standardOutput $ parallel_solve solve ( get_puzzles file) --unwords
+  --putStrLn $ unlines $ standardOutput $ parallel_solve format_solve ( get_puzzles file) --welcome to code jam
+  putStrLn $ unlines $ standardOutput $ parallel_solve format_solve ( get_puzzles file) -- alien solve
 
 welcome_solve sub_x [] = 0
 welcome_solve (sub_x: []) str_xs = length ( filter (\x -> x==sub_x) str_xs ) 
@@ -46,3 +47,25 @@ format_solve problem
              | solution < 1000 = getLast4 $ show $ 10000 + solution
              | otherwise  = getLast4 $ show solution
              where solution = welcome_solve "welcome to code jam" problem
+             
+----- Alien solve
+_alien_solve word_set input_string = filter (\x -> Set.member x word_set) $ get_words input_string
+
+splitOnFirst string_data char = _splitOnFirst [] string_data char
+_splitOnFirst dt [] char = (dt,[])
+_splitOnFirst dt (x:xs) char
+              | x == char = (dt, xs)
+              | otherwise = _splitOnFirst (dt ++ [x]) xs char
+
+get_words [] = []
+get_words (x:xs)
+       | x=='(' = mapLettersWords (get_words remainder) letters
+       | otherwise = mapLetterWords (get_words xs) x
+       where (letters,remainder) = splitOnFirst xs ')'
+
+
+mapLetter letter [] = [letter]
+mapLetter letter word   = letter:word
+mapLetterWords [] letter  = [[letter]] 
+mapLetterWords words letter = map (mapLetter letter) words
+mapLettersWords words letters = [ y | x <- (map (mapLetterWords words) letters), y<-x ]
